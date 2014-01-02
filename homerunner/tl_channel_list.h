64bb37ea456eca0ce095cf750e7c0f0d9494044b
Tristian Celestin <tristian.celestin@outlook.com>
2014-01-02 00:28:33 -0500

    Pasted header in wrong place.

--- homerunner/tl_channel_list.h
+++ homerunner/tl_channel_list.h
@@ -1,23 +1,3 @@
-#ifndef TL_CHANNEL_LIST_H
-#define TL_CHANNEL_LIST_H
-
-#include <glib.h>
-#include <gtk/gtk.h>
-#ifndef HDHOMERUN_H
-#define HDHOMERUN_H
-#include <hdhomerun/hdhomerun.h>
-#endif
-
-G_BEGIN_DECLS
-
-#define TL_TYPE_CHANNEL_LIST				(tl_channel_list_get_type ())
-#define TL_CHANNEL_LIST(o)        			(G_TYPE_CHECK_INSTANCE_CAST ((o), TL_TYPE_CHANNEL_LIST, TLChannelList))
-#define TL_CHANNEL_LIST_CLASS(k)			(G_TYPE_CHECK_CLASS_CAST ((k), TL_TYPE_CHANNEL_LIST, TLChannelListClass))
-#define TL_IS_CHANNEL_LIST(o)				(G_TYPE_CHECK_INSTANCE_TYPE ((o), TL_TYPE_CHANNEL_LIST))
-#define TL_IS_CHANNEL_LIST_CLASS(k)			(G_TYPE_CHECK_CLASS_TYPE ((k), TL_TYPE_CHANNEL_LIST))
-#define TL_CHANNEL_LIST_GET_CLASS(o)       	(G_TYPE_INSTANCE_GET_CLASS ((o), TL_TYPE_CHANNEL_LIST, TLChannelListClass))
-
-typedef struct _TLChannelList TLChannelList;
 /****************************************************************************
 **
 ** Copyright (C) 2013 Tristian Celestin
@@ -39,6 +19,26 @@ typedef struct _TLChannelList TLChannelList;
 **
 ****************************************************************************/
 
+#ifndef TL_CHANNEL_LIST_H
+#define TL_CHANNEL_LIST_H
+
+#include <glib.h>
+#include <gtk/gtk.h>
+#ifndef HDHOMERUN_H
+#define HDHOMERUN_H
+#include <hdhomerun/hdhomerun.h>
+#endif
+
+G_BEGIN_DECLS
+
+#define TL_TYPE_CHANNEL_LIST				(tl_channel_list_get_type ())
+#define TL_CHANNEL_LIST(o)        			(G_TYPE_CHECK_INSTANCE_CAST ((o), TL_TYPE_CHANNEL_LIST, TLChannelList))
+#define TL_CHANNEL_LIST_CLASS(k)			(G_TYPE_CHECK_CLASS_CAST ((k), TL_TYPE_CHANNEL_LIST, TLChannelListClass))
+#define TL_IS_CHANNEL_LIST(o)				(G_TYPE_CHECK_INSTANCE_TYPE ((o), TL_TYPE_CHANNEL_LIST))
+#define TL_IS_CHANNEL_LIST_CLASS(k)			(G_TYPE_CHECK_CLASS_TYPE ((k), TL_TYPE_CHANNEL_LIST))
+#define TL_CHANNEL_LIST_GET_CLASS(o)       	(G_TYPE_INSTANCE_GET_CLASS ((o), TL_TYPE_CHANNEL_LIST, TLChannelListClass))
+
+typedef struct _TLChannelList TLChannelList;
 typedef struct _TLChannelListClass TLChannelListClass;
 typedef struct _TLChannelListPrivate TLChannelListPrivate;
 
