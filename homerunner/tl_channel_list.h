09c9dc932846ec143c790c64acb820d332d8244b
Tristin Celestin <tristian.celestin@gmail.com>
2014-01-01 22:30:38 -0500

    Added LGPL headers

--- homerunner/tl_channel_list.h
+++ homerunner/tl_channel_list.h
@@ -18,6 +18,27 @@ G_BEGIN_DECLS
 #define TL_CHANNEL_LIST_GET_CLASS(o)       	(G_TYPE_INSTANCE_GET_CLASS ((o), TL_TYPE_CHANNEL_LIST, TLChannelListClass))
 
 typedef struct _TLChannelList TLChannelList;
+/****************************************************************************
+**
+** Copyright (C) 2013 Tristian Celestin
+** All rights reserved.
+** Contact: tristian.celestin@outlook.com
+**
+** This file is part of the Homerunner plugin.
+**
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** If you have questions regarding the use of this file, please contact
+** Tristian Celestin at tristian.celestin@outlook.com
+**
+****************************************************************************/
+
 typedef struct _TLChannelListClass TLChannelListClass;
 typedef struct _TLChannelListPrivate TLChannelListPrivate;
 
--- homerunner/tl_hdhr_playback_manager.h
+++ homerunner/tl_hdhr_playback_manager.h
@@ -1,3 +1,24 @@
+/****************************************************************************
+**
+** Copyright (C) 2013 Tristian Celestin
+** All rights reserved.
+** Contact: tristian.celestin@outlook.com
+**
+** This file is part of the Homerunner plugin.
+**
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** If you have questions regarding the use of this file, please contact
+** Tristian Celestin at tristian.celestin@outlook.com
+**
+****************************************************************************/
+
 #ifndef TL_HDHR_PLAYBACK_MANAGER_H
 #define TL_HDHR_PLAYBACK_MANAGER_H
 
