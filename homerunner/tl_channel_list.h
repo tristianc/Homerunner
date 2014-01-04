050219602c6c9520719532c629bbad90c59a97a2
Tristian Celestin <tristian.celestin@outlook.com>
2014-01-03 23:35:04 -0500

    Indentation issue

--- homerunner/tl_channel_list.h
+++ homerunner/tl_channel_list.h
@@ -32,11 +32,11 @@
 G_BEGIN_DECLS
 
 #define TL_TYPE_CHANNEL_LIST				(tl_channel_list_get_type ())
-#define TL_CHANNEL_LIST(o)        			(G_TYPE_CHECK_INSTANCE_CAST ((o), TL_TYPE_CHANNEL_LIST, TLChannelList))
+#define TL_CHANNEL_LIST(o)					(G_TYPE_CHECK_INSTANCE_CAST ((o), TL_TYPE_CHANNEL_LIST, TLChannelList))
 #define TL_CHANNEL_LIST_CLASS(k)			(G_TYPE_CHECK_CLASS_CAST ((k), TL_TYPE_CHANNEL_LIST, TLChannelListClass))
 #define TL_IS_CHANNEL_LIST(o)				(G_TYPE_CHECK_INSTANCE_TYPE ((o), TL_TYPE_CHANNEL_LIST))
 #define TL_IS_CHANNEL_LIST_CLASS(k)			(G_TYPE_CHECK_CLASS_TYPE ((k), TL_TYPE_CHANNEL_LIST))
-#define TL_CHANNEL_LIST_GET_CLASS(o)       	(G_TYPE_INSTANCE_GET_CLASS ((o), TL_TYPE_CHANNEL_LIST, TLChannelListClass))
+#define TL_CHANNEL_LIST_GET_CLASS(o)		(G_TYPE_INSTANCE_GET_CLASS ((o), TL_TYPE_CHANNEL_LIST, TLChannelListClass))
 
 typedef struct _TLChannelList TLChannelList;
 typedef struct _TLChannelListClass TLChannelListClass;
@@ -58,7 +58,7 @@ struct _TLChannelListClass
 	void (*set_device)(TLChannelList *self, guint32 device_id, gpointer user_data);
 };
 
-GtkWidget *	tl_channel_list_new();
+GtkWidget * tl_channel_list_new();
 void tl_channel_list_set_channel_model(TLChannelList *self, GtkListStore *channel_store);
 GtkListStore * tl_channel_list_get_channel_model(TLChannelList *self);
 void tl_channel_list_set_device_model(TLChannelList *self, GtkListStore *device_store);
